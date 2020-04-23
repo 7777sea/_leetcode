f2d92047939604be7476e06859b6695b230efbae
leo <923384777@qq.com>
2020-04-23 09:32:01 +0800

    4-23 problem solving

---
+++ src/pages/Demo/leetcode/DataStructure4-23.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const DataStructure = () => {
+
+    const problem = () => {
+        return `给定一个只包括 '(' ，')' ，'{' ，'}' ，'[' ，']' 的字符串，判断字符串是否有效。
+
+        有效字符串需满足：
+        
+        左括号必须用相同类型的右括号闭合。
+        左括号必须以正确的顺序闭合。
+        注意空字符串可被认为是有效字符串。
+        
+        示例 1:
+        
+        输入: "()"
+        输出: true
+        示例 2:
+        
+        输入: "()[]{}"
+        输出: true
+        示例 3:
+        
+        输入: "(]"
+        输出: false
+        示例 4:
+        
+        输入: "([)]"
+        输出: false
+        示例 5:
+        
+        输入: "{[]}"
+        输出: true`
+    }
+
+    const answer = (s) => {
+        const map = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+        let _ = [];
+        for(var i of s){
+            if(map[i]){_.push(i)}
+            else {
+                if(i != map[_.pop()]) return false;
+            }
+        }
+        return !_.length
+    }
+    
+    return <div className='dataStructure'> 
+        
+        {/*language=SCSS*/}
+        <style jsx>{`
+            .dataStructure{
+
+            }
+        `}</style>
+    </div>
+}
+
+export default DataStructure;
