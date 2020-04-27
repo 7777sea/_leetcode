412c9e13f30dad9976ecfed0e8dc290d061c6c18
leo <923384777@qq.com>
2020-04-27 09:27:36 +0800

    4-27 problem solving

---
+++ src/pages/Demo/leetcode/DataStructure4-27.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const DataStructure = () => {
+
+    const problem = () => {
+        return `给你一个字符串 s，「k 倍重复项删除操作」将会从 s 中选择 k 个相邻且相等的字母，并删除它们，使被删去的字符串的左侧和右侧连在一起。
+
+        你需要对 s 重复进行无限次这样的删除操作，直到无法继续为止。
+        
+        在执行完所有删除操作后，返回最终得到的字符串。
+        
+        本题答案保证唯一。
+        
+        示例 1：
+        
+        输入：s = "abcd", k = 2
+        输出："abcd"
+        解释：没有要删除的内容。
+        示例 2：
+        
+        输入：s = "deeedbbcccbdaa", k = 3
+        输出："aa"
+        解释： 
+        先删除 "eee" 和 "ccc"，得到 "ddbbbdaa"
+        再删除 "bbb"，得到 "dddaa"
+        最后删除 "ddd"，得到 "aa"
+        示例 3：
+        
+        输入：s = "pbbcggttciiippooaais", k = 2
+        输出："ps"
+        提示：
+        
+        1 <= s.length <= 10^5
+        2 <= k <= 10^4
+        s 中只含有小写英文字母。`
+    }
+
+
+    const answer = (s, k) => {
+
+        let stack = [];
+        for(let _s of s){
+            const last = stack.pop();
+            if(!last || last[0] !== _s){
+                stack.push(last)
+                stack.push(_s)
+            }else if(last.length < k-1){
+                stack.push(last + _s)
+            }
+        }
+    
+        return stack.join('')
+    }
+    
+    return <div className='dataStructure'>   
+        
+        
+        {/*language=SCSS*/}
+        <style jsx>{`
+            .dataStructure{
+
+            }
+        `}</style>
+    </div>
+}
+
+export default DataStructure;
