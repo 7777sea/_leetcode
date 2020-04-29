c09292b38e93defc4cabfce4288a6402d0f3aa8b
leo <923384777@qq.com>
2020-04-29 09:36:16 +0800

    4-29 problem solving

---
+++ src/pages/Demo/leetcode/DataStructure4-29.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const DataStructure = () => {
+
+    const problem = () => {
+        return `
+            // 实现一个add方法，使计算结果能够满足如下预期：
+            add(1)(2)(3) = 6;
+            add(1, 2, 3)(4) = 10;
+            add(1)(2)(3)(4)(5) = 15;
+        `
+    }
+
+
+    const add = () => {
+        // 第一次执行时，定义一个数组专门用来存储所有的参数
+        var _args = Array.prototype.slice.call(arguments);
+
+        // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
+        var _adder = function() {
+            _args.push(...arguments);
+            return _adder;
+        };
+
+        // 利用toString隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回
+        _adder.toString = function () {
+            return _args.reduce(function (a, b) {
+                return a + b;
+            });
+        }
+        return _adder;
+    }
+    
+    return <div className='dataStructure'>   
+        
+        
+        {/*language=SCSS*/}
+        <style jsx>{`
+            .dataStructure{
+
+            }
+        `}</style>
+    </div>
+}
+
+export default DataStructure;
